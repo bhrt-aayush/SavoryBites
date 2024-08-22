4fa381f68d3fdf29843306500e204ba3a80d2a7b
xela-ale <bhrt.aayush@gmail.com>
2024-08-22 16:47:15 +0545

    Basic Project Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <h1 className='bg-green'>Hiww!</h1>
+    </>
+  )
+}
+
+export default App
---
+++ client/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  theme: {
+    extend: {
+      colors:{
+        "green" :"#7dab8b",
+        "red" : "#cc363e",
+        "secondary" : "#555",
+        "primaryBG" : "#ededed",
+      }
+    },
+  },
+  plugins: [],
+}
+
