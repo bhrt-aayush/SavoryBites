6ab84ebc024d4667cf206c967427f0c223ed662c
xela-ale <bhrt.aayush@gmail.com>
2024-08-24 13:05:07 +0545

    Banner Job

---
+++ client/src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className='section-container bg-gray-50'>
+      <div className='py-24'>Banner</div>
+    </div>
+  )
+}
+
+export default Banner
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -71,7 +71,7 @@ const Navbar = () => {
         viewBox="0 0 24 24"
         stroke="currentColor">
         <path
-          strokeLinecap="round"
+          strokeLinecap="round" 
           strokeLinejoin="round"
           strokeWidth="2"
           d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
