24ffefe1064e74965e6c6c06c90f64e6c3337aff
xela-ale <bhrt.aayush@gmail.com>
2024-08-25 19:55:59 +0545

    Working

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,10 +5,10 @@ import { FiPhoneCall } from "react-icons/fi";
 const Navbar = () => {
   const [isSticky, setSticky] = useState(false);
     
-     useEffect(() => {
+  useEffect(() => {
     const handleScroll = () => {
       const offset = window.scrollY;
-      if (offset> 0) {
+      if (offset > 0) {
         setSticky(true);
       } else {
         setSticky(false);
@@ -19,7 +19,6 @@ const Navbar = () => {
     return () => {
       window.removeEventListener('scroll', handleScroll);
     }
-    
   }, [])
      
   const navItems = <>
@@ -38,7 +37,7 @@ const Navbar = () => {
         <details>
           <summary>Services</summary>
           <ul className="p-2">
-            <li><a>Online Orderings</a></li>
+            <li><a>Online Ordering</a></li>
             <li><a>Home Delivery</a></li>
             <li><a>Food Tracking</a></li>
           </ul>
@@ -46,80 +45,81 @@ const Navbar = () => {
       </li>
       <li><a>Offers</a></li>
   </>  
+
   return (
-<header className="max-w-screen-2x1 container mx-auto transition-all duration-300 ease-in-out">
-<div className={'navbar xl:px-24 $ {isSticky ? "shadow-md bg-base-100 transition-all duration-300 ease-in-out " : "" }'}>
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5" 
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 6h16M4 12h8m-8 6h16" />
-        </svg>
-      </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        {navItems}
-      </ul>
-    </div>
-    <a href='/'>
-      <img src ={SB} alt="" className="h-12" />
-    </a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      {navItems}
-    </ul>
-  </div>
-  <div className="navbar-end"> 
-    {/*search*/}
-    <button className="btn btn-ghost btn-circle hidden lg:flex">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-5 w-5"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor">
-        <path
-          strokeLinecap="round" 
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-      </svg>
-    </button>
-    {/*cart*/}
-     <label tabIndex={0} role="button" className="btn btn-ghost btn-circle mr-3 lg:flex hidden items center justify-center">
-        <div className="indicator">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-          </svg>
-          <span className="badge badge-sm indicator-item">0</span>
+    <header className={`max-w-screen-2x1 container mx-auto transition-all duration-300 ease-in-out ${isSticky ? "fixed top-0 left-0 right-0 bg-white z-50" : ""}`}>
+      <div className={'navbar xl:px-24 transition-all duration-300 ease-in-out'}>
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5" 
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16" />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+              {navItems}
+            </ul>
           </div>
-    </label>
-    {/*button*/}
-    <a className="btn bg-green rounded-full px-6 text-white flex items-center gap-3"><FiPhoneCall />Contact</a>
-  </div>
-</div>
-</header>
-  )
+          <a href='/'>
+            <img src={SB} alt="Logo" className="h-12" />
+          </a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 space-x-4">
+            {navItems}
+          </ul>
+        </div>
+        <div className="navbar-end flex items-center space-x-4"> 
+          {/*search*/}
+          <button className="btn btn-ghost btn-circle hidden lg:flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path
+                strokeLinecap="round" 
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+          </button>
+          {/*cart*/}
+          <label tabIndex={0} role="button" className="btn btn-ghost btn-circle mr-3 lg:flex hidden items-center justify-center">
+            <div className="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+              </svg>
+              <span className="badge badge-sm indicator-item">0</span>
+            </div>
+          </label>
+          {/*button*/}
+          <a className="btn bg-green rounded-full px-6 text-white flex items-center gap-3"><FiPhoneCall />Contact</a>
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default Navbar
+export default Navbar;
---
+++ client/src/pages/home/SpecialDishes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SpecialDishes = () => {
+  return (
+    <div className='section-container my-20'>SpecialDishes</div>
+  )
+}
+
+export default SpecialDishes
