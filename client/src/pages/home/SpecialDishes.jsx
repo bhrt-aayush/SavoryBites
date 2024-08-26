2c84bdd0ac3cf81638433d4ee0dbce1c4b7f2f5e
xela-ale <bhrt.aayush@gmail.com>
2024-08-26 09:25:04 +0545

    Worked Finally

--- client/src/pages/home/SpecialDishes.jsx
+++ client/src/pages/home/SpecialDishes.jsx
@@ -1,8 +1,77 @@
-import React from 'react'
+import React from 'react';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const SpecialDishes = () => {
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+
   return (
-    <div className='section-container my-20'>SpecialDishes</div>
+    <div className='section-container my-20'>
+     <div className='text-left'>
+                <p className='subtitle'>Special Dishes</p>
+                <h2 className='title md:w-[540px]'>Savor the Extraordinary Dishes From The Menu</h2>
+    </div>
+    <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+        <div>
+          <h3>7</h3>
+        </div>
+        <div>
+          <h3>8</h3>
+        </div>
+      </Slider>
+    </div>
   )
 }
 
